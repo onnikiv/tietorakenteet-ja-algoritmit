c4faaa1b191edd21b7fea5e158215587dd02b8f6
onnik <onni.kivinen@gmail.com>
2025-09-19 20:08:00 +0300

    finishing assignment 02

---
+++ chapter-4/02_check_balance.py
@@ -0,0 +1,25 @@
+def check_balance(text):
+    opened = "([{"
+    closed = ")]}"
+    pairs = {')': '(', ']': '[', '}': '{'}
+
+    stack = Stack()
+    count = 0
+
+    # Tää oli kyllä aivan järkyttävää
+    for i, char in enumerate(text):
+        if char in opened:
+            stack.push((char, i))
+        elif char in closed:
+            if len(stack) == 0:
+                return f"Match error at position {i}"
+            top, pos = stack.pop()
+            if pairs[char] != top:
+                return f"Match error at position {i}"
+            count += 1
+
+    if len(stack) > 0:
+        _, pos = stack.pop()
+        return f"Match error at position {pos}"
+
+    return f"Ok - {count}"
